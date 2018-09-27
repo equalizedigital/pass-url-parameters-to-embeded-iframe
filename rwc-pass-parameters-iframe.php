e7d00f19eae58d98b33377ac6444f8cb00ffaee3
Amber Hinds <amber@roadwarriorcreative.com>
2018-09-27 12:46:18 -0500

    Add functions file

---
+++ rwc-pass-parameters-iframe.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @link              https://roadwarriorcreative.com
+ * @since             1.0.0
+ * @package           RWC_Pass_Parameters_iframe
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Pass URL Parameters to Embeded iFrame
+ * Plugin URI:        https://roadwarriorcreative.com
+ * Description:       Allows parameters to pass from the embeded page URL to iframe source URL
+ * Version:           1.0.0
+ * Author:            Road Warrior Creative
+ * Author URI:        https://roadwarriorcreative.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       rwc-pass-parameters-iframe
+ * Domain Path:       /languages
+ */
+
+
+
+//* That's all!
