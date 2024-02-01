ee43d3dc7c7422927ac0751ae083eed9692eb395
Steve Jones <steve@stevejonesdev.com>
2024-02-01 00:36:27 -0500

    fixed - eslint

--- src/js/frontend.js
+++ src/js/frontend.js
@@ -1,27 +1,17 @@
-/**
- * This script passes URL parameters to iframes with specified ids
- *
- * @author Road Warrior Creative
- * @link https://roadwarriorcreative.com
- * @package Pass_Parameters_iframe
- * @version 1.0.1
- * @license GPL-2.0+
- */
- 
- jQuery(function($){
-	$(window).load(function() {
-		var iframe = document.getElementById("rwcGetParams");
+jQuery(function ($) {
+	$(window).load(function () {
+		const iframe = document.getElementById('rwcGetParams');
 		if (document.contains(iframe)) {
-			var loc = window.location.toString(),
-					params = loc.split('?')[1],
-					query;
-			if (params){
-				if( iframe.src.indexOf('?') >= 0){
+			const loc = window.location.toString();
+			const params = loc.split('?')[1];
+			let query;
+			if (params) {
+				if (iframe.src.indexOf('?') >= 0) {
 					query = '&';
-				}else{
+				} else {
 					query = '?';
 				}
-				newsrc = iframe.src + query + params;
+				const newsrc = iframe.src + query + params;
 				$('#rwcGetParams').attr('src', newsrc);
 			}
 		}
